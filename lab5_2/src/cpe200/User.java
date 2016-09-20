c39e80ee451d7961f2828be4059e20908db5c631
Eakkaphop Wongsorn <eakkaphop.w@gmail.com>
2016-09-20 18:47:51 +0700

    Update User.java

--- lab5_2/src/cpe200/User.java
+++ lab5_2/src/cpe200/User.java
@@ -4,26 +4,52 @@ package cpe200;
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    private String userName = null;
+    private String password;
 
     public User() {
     }
 
-    public String setUserName(String name) {
+    public String setUserName(String name) throws Exception {
+        String tmp = this.userName;
+        String regex = "[A-Za-z][A-Za-z0-9]{7,}$";
+        if(name.matches(regex)) {
+            this.userName = name;
+        }
+        else
+            throw new RuntimeException();
+        try{
+            return tmp;
+        }
+        catch (RuntimeException ex){
+
+
+        }
         return null;
     }
 
     public int setPassword(String name) {
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]{12,})$";
+        if(name.matches(regex)) {
+            this.password = name;
+        }
+        else
+            throw new RuntimeException();
+        try{
+            return  this.password.length();
+        }
+        catch (RuntimeException ex){
+
+        }
         return 0;
     }
 
     public String getUserName() {
 
-        return null;
+        return this.userName;
     }
 
     public String getPassword() {
-        return null;
+        return this.password;
     }
 }
