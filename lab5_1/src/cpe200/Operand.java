970ff2d8544037838f108ced61d7686c5a927c58
Eakkaphop Wongsorn <eakkaphop.w@gmail.com>
2016-09-20 18:46:59 +0700

    Update Operand.java

--- lab5_1/src/cpe200/Operand.java
+++ lab5_1/src/cpe200/Operand.java
@@ -4,11 +4,18 @@ public class Operand {
     private String operand;
 
     public Operand(String operand) {
+        this.operand = operand;
     }
 
     public Operand(int operand) {
+        this.operand = Integer.toString(operand);
     }
 
     public Operand(double operand) {
+        this.operand = Double.toString(operand);
     }
+    public String getValue() {
+        return  operand;
+    }
+
 }
